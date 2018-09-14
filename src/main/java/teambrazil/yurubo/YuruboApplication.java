2bc0c006337700608fc251d6cb1902630db46df5
namamono1129 <namamono1129v@gmail.com>
2018-09-14 09:41:06 +0900

    first commit

---
+++ src/main/java/teambrazil/yurubo/YuruboApplication.java
@@ -0,0 +1,12 @@
+package teambrazil.yurubo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuruboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuruboApplication.class, args);
+	}
+}
